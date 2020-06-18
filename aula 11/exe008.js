076bb888497b5698415ad8c84e081b7d2dcf6212
Marduk <anderson.worst@gmail.com>
2020-06-18 19:37:25 -0300

    .

---
+++ aula 11/exe008.js
@@ -0,0 +1,8 @@
+var velocidade = 70
+
+console.log(`A velocidade do seu carro é de ${velocidade} KM/H`);
+
+if(velocidade > 60){
+    console.log('Você ultrapassou o limite de velocidade e esta MULTADO');
+}
+console.log('Ultilize sempre o cinto de segurança')
---
+++ aula 11/exe009.js
@@ -0,0 +1,9 @@
+var pais = 'Brasil'
+
+console.log(`Vivendo no ${pais}`);
+
+if(pais != 'Brasil'){
+    console.log('Você é estrangeiro(a) burguês safado');
+}else{
+    console.log('Você é brasileiro(a) pé rapado');
+}
